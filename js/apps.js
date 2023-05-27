8bc038c3680049480abc758d52542d2d87f4cbae
md minhaz <106674774+minhazuddinWHOareyou223344@users.noreply.github.com>
2023-05-28 02:35:51 +0600

    Add files via upload

---
+++ js/apps.js
@@ -0,0 +1,160 @@
+$(function(){
+  var mixer = mixitup('.ourimg');
+   AOS.init();
+   //baner slider
+   $('#baner').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+   });
+   $('#testimonial').slick({
+    slidesToShow: 2,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+   });
+   $('#plan').slick({
+    slidesToShow: 5,
+    autoplay: true,
+   autoplaySpeed: 700,
+   prevArrow:"#plans .arrows  span.laftarrows",
+   nextArrow:"#plans .arrows  span.rightarrows",
+   dots: false,
+   responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+   });
+   $('#member').slick({
+    slidesToShow: 3,
+   autoplay: true,
+   autoplaySpeed: 700,
+   arrows: false,
+   dots: false,
+   responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+   });
+ $('#team').slick({
+   slidesToShow: 4,
+   autoplay: true,
+   autoplaySpeed: 700,
+   arrows: false,
+   dots: false,
+   responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+   
+ })
+  
+})
+new VenoBox({
+   selector: '.boss',
+   share:true,
+   spinner:'flow' ,
+  
+});
